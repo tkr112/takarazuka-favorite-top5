4a9e6f4a9d2cb66bbf9ab0317b9ff7b654d3d465
tkr112 <sumire.t110@gmail.com>
2026-09-22 09:43:53 +0900

    Update members.js

--- members.js
+++ members.js
@@ -1,1 +1,114 @@
+const members = [
 
+  // =====================
+  // 花組 🌸
+  // =====================
+
+  { id: 1,  name: "永久輝せあ", group: "花組", image: "images/hanagumi/1.JPEG" },
+  { id: 2,  name: "星空美咲", group: "花組", image: "images/hanagumi/2.JPEG" },
+  { id: 6,  name: "紫門ゆりや", group: "花組", image: "images/hanagumi/6.JPEG" },
+  { id: 10, name: "聖乃あすか", group: "花組", image: "images/hanagumi/10.JPEG" },
+  { id: 11, name: "一之瀬航季", group: "花組", image: "images/hanagumi/11.JPEG" },
+  { id: 12, name: "極美慎", group: "花組", image: "images/hanagumi/12.JPEG" },
+  { id: 18, name: "侑輝大弥", group: "花組", image: "images/hanagumi/18.JPEG" },
+  { id: 22, name: "朝葉ことの", group: "花組", image: "images/hanagumi/22.JPEG" },
+  { id: 24, name: "希波らいと", group: "花組", image: "images/hanagumi/24.JPEG" },
+  { id: 29, name: "天城れいん", group: "花組", image: "images/hanagumi/29.JPEG" },
+  { id: 30, name: "美羽愛", group: "花組", image: "images/hanagumi/30.JPEG" },
+  { id: 33, name: "美空真瑠", group: "花組", image: "images/hanagumi/33.JPEG" },
+  { id: 34, name: "夏希真斗", group: "花組", image: "images/hanagumi/34.JPEG" },
+  { id: 36, name: "初音夢", group: "花組", image: "images/hanagumi/36.JPEG" },
+  { id: 39, name: "鏡星珠", group: "花組", image: "images/hanagumi/39.JPEG" },
+  { id: 46, name: "七彩はづき", group: "花組", image: "images/hanagumi/46.JPEG" },
+
+
+  // =====================
+  // 月組 🌙
+  // =====================
+
+  { id: 78,  name: "鳳月杏", group: "月組", image: "images/tsukigumi/78.JPG" },
+  { id: 79,  name: "天紫珠李", group: "月組", image: "images/tsukigumi/79.JPG" },
+  { id: 83,  name: "英かおと", group: "月組", image: "images/tsukigumi/83.JPG" },
+  { id: 84,  name: "風間柚乃", group: "月組", image: "images/tsukigumi/84.JPG" },
+  { id: 87,  name: "礼華はる", group: "月組", image: "images/tsukigumi/87.JPG" },
+  { id: 89,  name: "彩海せら", group: "月組", image: "images/tsukigumi/89.JPG" },
+  { id: 93,  name: "白河りり", group: "月組", image: "images/tsukigumi/93.JPG" },
+  { id: 101, name: "七城雅", group: "月組", image: "images/tsukigumi/101.JPG" },
+  { id: 107, name: "花妃舞音", group: "月組", image: "images/tsukigumi/107.JPG" },
+  { id: 113, name: "美颯りひと", group: "月組", image: "images/tsukigumi/113.JPG" },
+  { id: 122, name: "雅耀", group: "月組", image: "images/tsukigumi/122.JPG" },
+  { id: 128, name: "乃々れいあ", group: "月組", image: "images/tsukigumi/128.JPG" },
+  { id: 126, name: "帆華なつ海", group: "月組", image: "images/tsukigumi/126.JPG" },
+  { id: 137, name: "薫乃咲月", group: "月組", image: "images/tsukigumi/137.JPG" },
+
+
+  // =====================
+  // 雪組 ❄️
+  // =====================
+
+  { id: 150, name: "朝美絢", group: "雪組", image: "images/yukigumi/150.JPEG" },
+  { id: 151, name: "音彩唯", group: "雪組", image: "images/yukigumi/151.JPEG" },
+  { id: 156, name: "瀬央ゆりあ", group: "雪組", image: "images/yukigumi/156.JPEG" },
+  { id: 158, name: "諏訪さき", group: "雪組", image: "images/yukigumi/158.JPEG" },
+  { id: 160, name: "縣千", group: "雪組", image: "images/yukigumi/160.JPEG" },
+  { id: 162, name: "咲城けい", group: "雪組", image: "images/yukigumi/162.JPEG" },
+  { id: 167, name: "蒼波黎也", group: "雪組", image: "images/yukigumi/167.JPEG" },
+  { id: 173, name: "紀城ゆりや", group: "雪組", image: "images/yukigumi/173.JPEG" },
+  { id: 180, name: "華世京", group: "雪組", image: "images/yukigumi/180.JPEG" },
+  { id: 181, name: "華純沙那", group: "雪組", image: "images/yukigumi/181.JPEG" },
+  { id: 187, name: "白綺華", group: "雪組", image: "images/yukigumi/187.JPEG" },
+  { id: 193, name: "苑利香輝", group: "雪組", image: "images/yukigumi/193.JPEG" },
+  { id: 195, name: "星沢ありさ", group: "雪組", image: "images/yukigumi/195.JPEG" },
+  { id: 202, name: "律希奏", group: "雪組", image: "images/yukigumi/202.JPEG" },
+  { id: 217, name: "澄花まい", group: "雪組", image: "images/yukigumi/217.JPEG" },
+
+
+  // =====================
+  // 星組 ⭐️
+  // =====================
+
+  { id: 223, name: "暁千星", group: "星組", image: "images/hoshigumi/223.JPEG" },
+  { id: 224, name: "詩ちづる", group: "星組", image: "images/hoshigumi/224.JPEG" },
+  { id: 228, name: "瑠風輝", group: "星組", image: "images/hoshigumi/228.JPEG" },
+  { id: 234, name: "碧海さりお", group: "星組", image: "images/hoshigumi/234.JPEG" },
+  { id: 237, name: "天飛華音", group: "星組", image: "images/hoshigumi/237.JPEG" },
+  { id: 241, name: "瑠璃花夏", group: "星組", image: "images/hoshigumi/241.JPEG" },
+  { id: 247, name: "御剣海", group: "星組", image: "images/hoshigumi/247.JPEG" },
+  { id: 250, name: "稀惺かずと", group: "星組", image: "images/hoshigumi/250.JPEG" },
+  { id: 252, name: "大希颯", group: "星組", image: "images/hoshigumi/252.JPEG" },
+  { id: 257, name: "乙華菜乃", group: "星組", image: "images/hoshigumi/257.JPEG" },
+  { id: 266, name: "碧羽陽", group: "星組", image: "images/hoshigumi/266.JPEG" },
+  { id: 268, name: "藍羽ひより", group: "星組", image: "images/hoshigumi/268.JPEG" },
+  { id: 270, name: "茉莉那ふみ", group: "星組", image: "images/hoshigumi/270.JPEG" },
+  { id: 272, name: "馳琉輝", group: "星組", image: "images/hoshigumi/272.JPEG" },
+
+
+  // =====================
+  // 宙組 🪐
+  // =====================
+
+  { id: 301, name: "桜木みなと", group: "宙組", image: "images/soragumi/301.JPG" },
+  { id: 302, name: "春乃さくら", group: "宙組", image: "images/soragumi/302.JPG" },
+  { id: 303, name: "水美舞斗", group: "宙組", image: "images/soragumi/303.JPG" },
+  { id: 310, name: "天彩峰里", group: "宙組", image: "images/soragumi/310.JPG" },
+  { id: 311, name: "鷹翔千空", group: "宙組", image: "images/soragumi/311.JPG" },
+  { id: 315, name: "風色日向", group: "宙組", image: "images/soragumi/315.JPG" },
+  { id: 319, name: "亜音有星", group: "宙組", image: "images/soragumi/319.JPG" },
+  { id: 320, name: "二葉ゆゆ", group: "宙組", image: "images/soragumi/320.JPG" },
+  { id: 322, name: "きよら羽龍", group: "宙組", image: "images/soragumi/322.JPG" },
+  { id: 325, name: "大路りせ", group: "宙組", image: "images/soragumi/325.JPG" },
+  { id: 326, name: "泉堂成", group: "宙組", image: "images/soragumi/326.JPG" },
+  { id: 327, name: "山吹ひばり", group: "宙組", image: "images/soragumi/327.JPG" },
+  { id: 329, name: "美星帆那", group: "宙組", image: "images/soragumi/329.JPG" },
+  { id: 330, name: "鳳城のあん", group: "宙組", image: "images/soragumi/330.JPG" },
+  { id: 343, name: "梨恋あやめ", group: "宙組", image: "images/soragumi/343.JPG" },
+  { id: 344, name: "花恋こまち", group: "宙組", image: "images/soragumi/344.JPG" },
+
+
+  // =====================
+  // 専科 ✨
+  // =====================
+
+  { id: 388, name: "小桜ほのか", group: "専科", image: "images/senka/388.JPEG" }
+
+];
